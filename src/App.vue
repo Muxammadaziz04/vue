b94735df7e89b4c0f7c09cfdaa21b9919485a7c0
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2022-10-16 13:23:54 +0500

    слоты, refs, жизненный цикл компонентов

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <div class="p-3">
+    <button
+      type="button"
+      class="btn btn-primary"
+      @click="openModal"
+    >
+      Подробнее
+    </button>
+    <modal
+      v-if="isModalOpen"
+      title="Пользовательское соглашение"
+      @close="isModalOpen = false"
+    >
+      <div class="information">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, repellendus fugit. Perferendis molestiae reiciendis magni minus, saepe voluptatem. Laborum temporibus quas eos minus placeat saepe nisi nesciunt distinctio optio fugit.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, repellendus fugit. Perferendis molestiae reiciendis magni minus, saepe voluptatem. Laborum temporibus quas eos minus placeat saepe nisi nesciunt distinctio optio fugit.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, repellendus fugit. Perferendis molestiae reiciendis magni minus, saepe voluptatem. Laborum temporibus quas eos minus placeat saepe nisi nesciunt distinctio optio fugit.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, repellendus fugit. Perferendis molestiae reiciendis magni minus, saepe voluptatem. Laborum temporibus quas eos minus placeat saepe nisi nesciunt distinctio optio fugit.
+        </p>
+      </div>
+    </modal>
+  </div>
+  </div>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  components: { Modal },
+  data() {
+    return {
+      isModalOpen: false
+    }
+  },
+  methods: {
+    openModal() {
+      this.isModalOpen = true
+    }
+  }
+}
+</script>
